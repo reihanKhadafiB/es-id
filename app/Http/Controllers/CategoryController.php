8b2a2968dfa5816cdf14068e75d6af69f1bb709e
reihanKhadafiB <reihankhadafi202@gmail.com>
2026-09-11 10:23:32 +0700

    refactor(controller): reorder use statements in CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
