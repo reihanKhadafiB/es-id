a580787f23c8002136c279ca38b05f0da49fc12f
reihanKhadafiB <reihankhadafi202@gmail.com>
2026-09-11 10:33:30 +0700

    fix(tests): update authentication tests to use phone number instead of email

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -14,7 +14,7 @@ test('users can authenticate using the login screen', function () {
     $user = User::factory()->create();
 
     $response = $this->post(route('login.store'), [
-        'email' => $user->email,
+        'phone_number' => $user->phone_number,
         'password' => 'password',
     ]);
 
@@ -33,7 +33,7 @@ test('users with two factor enabled are redirected to two factor challenge', fun
     $user = User::factory()->withTwoFactor()->create();
 
     $response = $this->post(route('login'), [
-        'email' => $user->email,
+        'phone_number' => $user->phone_number,
         'password' => 'password',
     ]);
 
@@ -46,7 +46,7 @@ test('users can not authenticate with invalid password', function () {
     $user = User::factory()->create();
 
     $this->post(route('login.store'), [
-        'email' => $user->email,
+        'phone_number' => $user->phone_number,
         'password' => 'wrong-password',
     ]);
 
@@ -66,10 +66,10 @@ test('users can logout', function () {
 test('users are rate limited', function () {
     $user = User::factory()->create();
 
-    RateLimiter::increment(md5('login'.implode('|', [$user->email, '127.0.0.1'])), amount: 5);
+    RateLimiter::increment(md5('login'.implode('|', [$user->phone_number, '127.0.0.1'])), amount: 5);
 
     $response = $this->post(route('login.store'), [
-        'email' => $user->email,
+        'phone_number' => $user->phone_number,
         'password' => 'wrong-password',
     ]);
 
