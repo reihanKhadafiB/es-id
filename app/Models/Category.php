3afd0113b2877f7e46bf57314135a9fbd856ed11
reihanKhadafiB <reihankhadafi202@gmail.com>
2026-09-11 10:19:47 +0700

    refactor(model): add return type hint for products method in Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     protected $fillable = ['name'];
 
-    public function products()
+    /** @return HasMany<Product, $this> */
+    public function products(): HasMany
     {
         return $this->hasMany(Product::class);
     }
